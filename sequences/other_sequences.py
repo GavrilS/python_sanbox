ab9cf70e92cdd60ed2a5e87202d411b892f79d81
GavrilS <gavr1lll@abv.bg>
2023-10-26 08:20:58 +0300

    Deque tests

--- sequences/other_sequences.py
+++ sequences/other_sequences.py
@@ -1,10 +1,12 @@
 """
 If the sequence will only contain numbers, it could be more efficient to use array.array instead of list.
 Another fast and flexible way of saving data is with the pickle module(it handles almost all built in types).
+Besides deque, other python standard library packages implement queues: queue, multiprocessing, asyncio, heapq
 """
 from argparse import ArgumentParser
 from array import array
 from random import random
+from collections import deque
 
 parser = ArgumentParser(description='This is going to be used to specify which specific sequence type tests to run')
 
@@ -41,6 +43,20 @@ def memory_view_ops():
     print('numbers: ', numbers)
 
 
+def deque_ops():
+    dq = deque(range(10), maxlen=10)
+    print('dq: ', dq)
+    dq.rotate(3)
+    print('dq.rotate(3): ', dq)
+    dq.rotate(-4)
+    print('dq.rotate(-4): ', dq)
+    dq.appendleft(-1)
+    print('dq.appendleft(-1): ', dq)
+    dq.extend([11, 22, 33])
+    print('dq.extend([11, 22, 33]): ', dq)
+    dq.extendleft([10, 20, 30, 40])
+    print('dq.extendleft([10, 20, 30, 40]): ', dq)
+
 
 if __name__=='__main__':
     if seq_type == 'array':
@@ -48,4 +64,4 @@ if __name__=='__main__':
         print('*'*40)
         memory_view_ops()
     elif seq_type == 'deque':
-        pass
+        deque_ops()
