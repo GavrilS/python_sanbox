a98c0be3c5d5cf180a8896296e13d5f1d17fd867
GavrilS <gavr1lll@abv.bg>
2023-12-06 08:34:33 +0200

    Added more decorator tests

--- function_decorators/basic_decorator_exmpl.py
+++ function_decorators/basic_decorator_exmpl.py
@@ -10,6 +10,20 @@ def target():
     print('running target()')
 
 
+def deco_test(func):
+    print('This is deco test')
+    return func
+
+
+@deco_test
+def test():
+    print('running test()')
+
+
 if __name__=='__main__':
     target()
     print('target: ', target)
+    print('*'*40)
+
+    test()
+    print('test: ', test)
