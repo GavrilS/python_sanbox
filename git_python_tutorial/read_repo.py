870d1ec0ed4094b1dfc08a3180908f495109b491
GavrilS <gavr1lll@abv.bg>
2024-05-09 22:37:18 +0300

    Added default repo path for the tutorial

--- git_python_tutorial/read_repo.py
+++ git_python_tutorial/read_repo.py
@@ -2,8 +2,9 @@ import os
 from git import Repo
 
 COMMITS_TO_PRINT = 5
+DEFAULT_REPO_PATH = '/tmp/python_sanbox'
 
-def print_commit(commit):
+def print_commit(commit): # print commit details
     print('------------')
     print(str(commit.hexsha)) # 40-character SHA-1 hash for the commit
     print("\"{}\" by {} ({})".format(
@@ -19,5 +20,24 @@ def print_commit(commit):
     )))
 
 
+def print_repository(repo): # print repo details
+    print("Repo description: {}".format(repo.description))
+    print("Repo active branch is {}".format(repo.active_branch))
+    for remote in repo.remotes:
+        print("Remote named '{}' with URL '{}'".format(remote, remote.url))
+    print('Last commit for Repo is {}.'.format(str(repo.head.commit.hexsha)))
+
+
 if __name__=='__main__':
-    pass
+    repo_path = os.getenv('GIT_REPO_PATH', DEFAULT_REPO_PATH)
+    repo = Repo(repo_path) # Repo object used to programmatically interact with Git repositories 
+    # Check the repository loaded correctly
+    if not repo.bare:
+        print('Repo at {} successfully loaded.'.format(repo_path))
+        print_repository(repo)
+        # create list of commits then print some of them to stdout
+        commits = list(repo.iter_commits('master'))[:COMMITS_TO_PRINT]
+        for commit in commits:
+            print_commit(commit)
+    else:
+        print('Could not load repository at {}.'.format(repo_path))
