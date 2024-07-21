57da779a9ebd433e2a4e6b757553a74c9c3d47d1
GavrilS <gavr1lll@abv.bg>
2024-07-21 21:07:12 +0300

    Coroutine test case scenarios with multiple usage of a routine based on a user input

---
+++ flow_control/coroutines/coroutines_test.py
@@ -0,0 +1,63 @@
+"""
+Python program for showing basic usage of coroutines.
+"""
+from argparse import ArgumentParser
+
+VALID_TEST_CASES = [
+    'single',
+    'continous_coro'
+]
+
+name = 'Initial'
+
+def print_matching_name(sub_name):
+    print(f"Searching for names containing: {sub_name}")
+    while True:
+        name = yield
+        if sub_name in name:
+            print(name)
+            break
+
+
+def single():
+    global name
+    sub_name = input('Give me a name to look for: ')
+    coro = print_matching_name(sub_name)
+    coro.__next__()
+
+    try:
+        name = 'Achilles'
+        coro.send(name)
+        name = 'Heracles'
+        coro.send(name)
+        name = 'Perseus'
+        coro.send(name)
+    except Exception as e:
+        print('Reached the end of the coroutine: ', str(e))
+        print('Finished on name: ', name)
+
+
+def continous_coro():
+    global name
+    sub_name = input('Give me a name to look for: ')
+
+    while True:
+        try:
+            coro = print_matching_name(sub_name)
+            coro.__next__()
+            name = input('Give me a name to test: ')
+            if name == 'exit':
+                return
+            coro.send(name)
+        except Exception as e:
+            print('Found the name we were looking for! ', str(e))
+
+
+if __name__=='__main__':
+    parser = ArgumentParser(description='Testing coroutines')
+    parser.add_argument('--test_case', default='single', choices=VALID_TEST_CASES)
+    args = parser.parse_args()
+    test_case = args.test_case
+
+    eval(test_case)()
+    print('After test case the final name value is: ', name)
