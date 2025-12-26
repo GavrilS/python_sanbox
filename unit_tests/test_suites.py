ceb54e6a5c9babba5ed20ec67ec9b4f8d4e972af
GavrilS <gavr1lll@abv.bg>
2025-12-26 14:51:40 +0200

    Preparing an example of test suites with unittest

--- unit_tests/test_suites.py
+++ unit_tests/test_suites.py
@@ -12,3 +12,21 @@ def check_substring(s, sub_s):
 def check_uppercase(s):
     return s == s.uppercase()
 
+
+class TestSuitesOfTests(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(1, 3), 4)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(1, 3), -2)
+
+    def test_check_substring(self):
+        self.assertEqual(check_substring('Test123', '123'), True)
+    
+    def test_check_uppercase(self):
+        self.assertEqual(check_uppercase('TEST123'), True)
+
+    
+if __name__=='__main__':
+    pass
