deffb2a6eb1ed328a3dc081cbc4272c8e8533a21
GavrilS <gavr1lll@abv.bg>
2025-12-29 19:26:09 +0200

    Added an example usage of pytest for testing python code

--- unit_tests/pytest/starter_guide.py
+++
@@ -1,6 +0,0 @@
-'''
-This is a starter guide to pytest.
-
-Install pytest with pip:
-    pip install -U pytest
-'''
---
+++ unit_tests/pytest/test_example.py
@@ -0,0 +1,51 @@
+'''
+This is a starter guide to pytest.
+
+Install pytest with pip:
+    pip install -U pytest
+    pytest --version
+
+Run all tests in current directory and all sub-directories (only applies to files starting with
+test_, functions starting or ending with test and classes starting with Test):
+    pytest
+    pytest -vv
+    pytest -q
+
+Flags:
+    -vv -> verbose
+    -q -> quiet
+'''
+import pytest
+
+# Test a function result
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 2) == 3
+
+# Test an exception is raised
+def throw_exc():
+    raise SystemExit(1)
+
+def test_raises():
+    with pytest.raises(SystemExit):
+        throw_exc()
+
+# Group multiple tests in a class
+class TestExample:
+
+    def test_one(self):
+        x = 'this'
+        assert 'h' in x
+
+    def test_two(self):
+        greeting = 'Hellow World!'
+        assert 'orld' in greeting
+
+# Compare floating-point values
+def calculate_floats(a, b):
+    return a + b
+
+def test_calculate_floats():
+    assert calculate_floats(0.1, 0.2) == pytest.approx(0.3)
